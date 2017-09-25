ad18c9f2ef3001878f265ab35eb4d91939d84a66
BestKora <developer@bestkora.com>
2017-09-25 16:27:58 +0300

    iOS 9 Xcode 9

--- Photomania/PhotographersCDTVC.m
+++ Photomania/PhotographersCDTVC.m
@@ -17,6 +17,7 @@
 
 - (void)awakeFromNib
 {
+    [super awakeFromNib];
     [[NSNotificationCenter defaultCenter] addObserverForName:PhotoDatabaseAvailabilityNotification
                                                       object:nil
                                                        queue:nil
